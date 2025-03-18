22294398482c7326c097646bdf201b0ab409b675
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-03-18 13:14:04 +0900

    Feat | 플레이어 피격 시 카메라쉐이킹 구현

--- Assets/02.Scripts/02-2. Player/Player.cs
+++ Assets/02.Scripts/02-2. Player/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public static Player Instance { get; private set; }
     [SerializeField] private int _maxHealthPoint = 3;
     [SerializeField] private int _currentHealthPoint;
+    [SerializeField] private ShakeCamera _shakeCamera;
 
     private PlayerData _playerData;
     public PlayerData PlayerData
@@ -31,5 +32,9 @@ public class Player : MonoBehaviour
     public void TakeDamage(int damage)
     {
         _playerData.CurrentHealthPoint -= damage;
+        if (!ReferenceEquals(_shakeCamera, null))
+        {
+            _shakeCamera.Shake(0.5f, 0.3f);
+        }
     }
 }
---
+++ Assets/02.Scripts/02-5. Util/ShakeCamera.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    private Vector3 _originalPosition;
+    private void Awake()
+    {
+        _originalPosition = transform.localPosition;
+    }
+    private IEnumerator ShakeCoroutine(float duration, float magnitude)
+    {
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = _originalPosition + new Vector3(x, y, 0);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = _originalPosition;
+    }
+
+    public void Shake(float duration, float magnitude)
+    {
+        StartCoroutine(ShakeCoroutine(duration, magnitude));
+    }
+}
