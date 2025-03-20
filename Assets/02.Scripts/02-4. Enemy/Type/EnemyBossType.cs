16eb394c9286891947259e819c092997f68e65e4
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-03-20 14:08:41 +0900

    Add | EnemyBossType 추가 및 타입별 Enemy 스크립트 폴더 정리

---
+++ Assets/02.Scripts/02-4. Enemy/Type/EnemyBossType.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EnemyBossType: Enemy
+{
+
+
+
+    public void AttackTypeA()
+    {
+        // 360µµ ÃÑ¾Ë
+    }
+
+    public void AttackTypeB()
+    {
+        
+    }
+
+    public void AttackTypeC()
+    {
+        
+    }
+}
