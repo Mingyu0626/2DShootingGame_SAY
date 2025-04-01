93282bfba87ecf0db505b59471b5948b45d2fd47
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-01 12:36:41 +0900

    Feat | UI_Attendance 구현
    출석일 패널에 부착되어 리워드 획득 및 출석보상 데이터를 가져와 ui를 초기화하는 역할을 담당한다.

--- Assets/02.Scripts/02-9. Environment/Attendance/UI/UI_Attendance.cs
+++ Assets/02.Scripts/02-9. Environment/Attendance/UI/UI_Attendance.cs
@@ -1,24 +1,62 @@
+using Coffee.UIExtensions;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Attendance : MonoBehaviour
 {
     [SerializeField]
-    private Button _attendanceButton;
+    private int _attendanceDay;
     [SerializeField]
-    private GameObject _attendancePopup;
+    private TextMeshProUGUI _rewardAmountTMP;
     [SerializeField]
-    private 
+    private Image _rewardImage;
+    [SerializeField]
+    private List<Sprite> _rewardSprite;
+    private Attendance _attendance;
 
+    [SerializeField]
+    private Button _getRewardButton;
+
+    private ShinyEffectForUGUI _particleEffect;
+    private float _effectDuration = 1f;
     private void Awake()
     {
-        _attendanceButton.onClick.AddListener(SetAttenDancePopupEnable);
+        _getRewardButton.onClick.AddListener(GetReward);
+        _particleEffect = GetComponent<ShinyEffectForUGUI>();
     }
-
-
-    private void SetAttenDancePopupEnable()
+    private void Start()
     {
-        _attendancePopup.SetActive(true);
+        _attendance = AttendanceManager.Instance.Attendances[_attendanceDay];
+        InitUI();
     }
+
+    private void InitUI()
+    {
+        _rewardImage.sprite = _rewardSprite[(int)_attendance.Data.RewardCurrencyType];
+        _rewardAmountTMP.text = _attendance.Data.RewardAmount.ToString();
+
+        if (AttendanceManager.Instance.TryGetReward(_attendance))
+        {
+            _getRewardButton.interactable = true;
+        }
+    }
+
+    private void GetReward()
+    {
+        CurrencyManager.Instance.Add
+            (_attendance.Data.RewardCurrencyType, _attendance.Data.RewardAmount);
+        _attendance.IsRewarded = true;
+        _getRewardButton.interactable = false;
+        AttendanceManager.Instance.Save();
+        ActivateParticleEffect();
+    }
+
+    private void ActivateParticleEffect()
+    {
+        _particleEffect.Play(_effectDuration);
+    }
+
 
 }
